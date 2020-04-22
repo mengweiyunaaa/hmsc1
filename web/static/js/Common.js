0b93453b08bdf7e1976b1cb353c7892c19a14c47
mwy <1476389698@qq.com>
2020-04-22 17:48:43 +0800

    '登录提示'

--- web/static/js/Common.js
+++ web/static/js/Common.js
@@ -2,15 +2,21 @@
 var common_ops = {
     init:function () {
         console.log('init的common.js')
+        this.eventBind()
+    },
+    eventBind:function () {
+        console.log('hahahah')
     },
     buildUrl:function (path,params) {
         var url = "" + path;
         console.log('--------');
         console.log('url'+ url);
-        console.log(1)
+        console.log(1);
+        return url
     }
 };
 $(document).ready(function () {
     common_ops.init();
-    common_ops.buildUrl()
+    common_ops.buildUrl();
+
 });
---
+++ web/static/js/user/login.js
@@ -0,0 +1,43 @@
+;
+var user_login_ops={
+    init:function () {
+        this.eventBind();
+    },
+    eventBind:function () {
+        $('.login_wrap #do-login').click(function () {
+            var login_name=$('.login_wrap input[name=login_name]').val()
+            var login_pwd=$('.login_wrap input[name=login_pwd]').val()
+            console.log(login_name);
+            console.log(login_pwd);
+            if (login_name == undefined || login_name.length < 1){
+                alert('name  x')
+                return
+            }
+             if (login_pwd == undefined || login_pwd.length < 1){
+                 alert('pwd  x')
+                 return;
+            }
+             $.ajax({
+                 url:common_ops.buildUrl('/user/login'),
+                 type:'POST',
+                 data:{
+                     'login_name':login_name,
+                     'login_pwd':login_pwd
+
+                 },
+                 dataType:'json',
+                 success:function (resp) {
+                     console.log(resp);
+                     alert(resp.msg)
+
+
+                 }
+             })
+        })
+    }
+}
+$(document).ready(function () {
+    user_login_ops.init();
+
+
+});
